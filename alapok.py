19de8b8041de7b7d8ba45dcd2d54cad8626966c9
CsEgee <csizmadia.gergely@hotmail.com>
2022-04-16 01:54:03 +0200

    Hi > Hello

---
+++ alapok.py
@@ -0,0 +1,18 @@
+# print("alma")
+#
+# name = "Robert"
+# print(type(name))
+# print(type(5))
+# print(type(name))
+# print("Szia " + name + "!")
+
+alap = 3
+kitevo = 4
+print(type(alap))
+print("az eredmény: " + str(alap ** kitevo))
+
+print(type(alap ** kitevo))
+
+kitevo = 0.5
+print(type(alap ** kitevo))
+print("az eredmény: " + str(round(alap ** kitevo, 2)))
